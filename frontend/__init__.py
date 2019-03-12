8b83e0cc0c8c2a6d6f9cf783e91755c9cc6ac4d4
doccaz <erico.mendonca@gmail.com>
2019-03-12 01:43:42 -0300

    using sqlalchemy; adding sample config

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import os
 import flask_login
+from flask_sqlalchemy import SQLAlchemy
 from backend.libvirtbridge import DomainQuery
 
 # translation support
@@ -10,18 +11,25 @@ try:
 except FileNotFoundError as e:
     DomainQuery.log("no translation file available")
 
-
-#app = Flask(__name__, instance_relative_config=True)
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 app.debug = False
+
+# load configuration
+import config as cfg
+for key, value in cfg.config.items():
+    app.config[key] = value
 
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
+db = SQLAlchemy(app)
+
 from frontend.auth.views import auth
 app.register_blueprint(auth)
+
+db.create_all()
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
--- labvirtual.py
+++ labvirtual.py
@@ -1,10 +1,6 @@
 import sys
-
 sys.path.insert(0, "/srv/www/labvirtual")
-
 from frontend import app as application
-import config
 
-#app.run(debug=True)
 application.run
 
